cf8ddbc21f11619726c0aa7f62d50ee21083b2f0
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-09-27 03:38:36 +0700

    playlist updated

--- app/lib/gist.ts
+++ app/lib/gist.ts
@@ -1,3 +1,3 @@
 export const gist = {
-  url: 'https://gist.githubusercontent.com/fauziralpiandi/d4c4fdc10d3494d4a0e7702198a0fca3/raw/5d54b869b0ebcc917cd9c16993894f98e0c36c6e/playlist.json'
+  url: 'https://gist.githubusercontent.com/fauziralpiandi/d4c4fdc10d3494d4a0e7702198a0fca3/raw/6d79d86e77e62f1b15560047e267761a63b75be5/playlist.json'
 }
