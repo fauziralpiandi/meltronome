fd2ec1333838114c08f539109790a6db42024d60
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-09-27 03:22:34 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,28 +9,8 @@ const alegreya = Alegreya({
 })
 
 export const metadata: Metadata = {
-  title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
-  },
-  description: 'This is my portfolio.',
-  openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
-    siteName: 'My Portfolio',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      'index': true,
-      'follow': true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
+  title: 'Meltronome - Fauzira Alpiandi'
+  description: '',
   },
 }
 
