772e1e7c791782df69c50e4b5af1c056aaf4898c
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-10-02 16:54:24 +0000

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,3 @@
-const securityHeaders = [
-  {
-    key: 'Referrer-Policy',
-    value: 'no-referrer-when-downgrade',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-];
-
-const cspHeader = `
-    default-src '*';
-    script-src '*';
-    style-src '*';
-    img-src '*';
-    font-src '*';
-    object-src '*';
-    base-uri '*';
-    form-action '*';
-    frame-ancestors '*';
-    upgrade-insecure-requests;
-`.replace(/\n/g, '');
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -41,31 +9,7 @@ const nextConfig = {
     ],
   },
   async headers() {
-    if (process.env.NODE_ENV === 'production') {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            ...securityHeaders,
-            {
-              key: 'Content-Security-Policy',
-              value: cspHeader,
-            },
-          ],
-        },
-      ];
-    }
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'noindex, nofollow',
-          },
-        ],
-      },
-    ];
+    return [];
   },
 }
 
